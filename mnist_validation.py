c06840e86518aec8b618010ea2b23a7aeca441a3
GoldmanDrew <dag5wd@virginia.edu>
2023-03-10 10:43:29 -0500

    updated code

--- mnist_validation.py
+++ mnist_validation.py
@@ -1,37 +1,30 @@
 from functools import total_ordering
-import time
-import os
-import hy
-import yaml
-import numpy as np
-import pandas as pd
-import random
-import pickle
+import time, os, hy, yaml
+import numpy as np, pandas as pd
+import random, pickle
 from tqdm import tqdm
 from mnist_NN import TrainingInstance
 from tensorflow import keras
-
 
 def remove_divisions(instring):
     resultstring = instring
     for i in range(len(instring)):
         if instring[i] == '/':
-            #             print(instring[])
+#             print(instring[])
             a = instring[i+2:].split(')')[0].split(' ')[0]
             b = instring[i+2:].split(')')[0].split(' ')[1]
             c = float(a)/float(b)
             resultstring = resultstring.replace(f'(/ {a} {b})', f'{c}', 1)
 #             print(instring[i+2:].find(')'))
-
+            
 #             print(f"a is {a}, b is {b}, c is {c}")
     return resultstring
-
 
 def nice_parenthesis(instring):
     result = ""
     currenttab = 0
     for i in instring:
-
+        
         if i == '(':
             currenttab += 1
             result += "(\n"
@@ -44,9 +37,8 @@ def nice_parenthesis(instring):
                 result += "\t"
             result += ")"
         else:
-            result += i
+            result +=i
     return result
-
 
 def get_first_correct_parenthesis(instring):
     num_parenthesis = 1
@@ -67,7 +59,6 @@ def get_first_correct_parenthesis(instring):
         elif instring[last_parenthesis] == ')':
             num_parenthesis -= 1
     return instring[first_parenthesis:last_parenthesis+1]
-
 
 def get_second_correct_parenthesis(instring):
     num_parenthesis = 1
@@ -90,7 +81,6 @@ def get_second_correct_parenthesis(instring):
     # print(f"Sent to find first parenthesis: {instring[last_parenthesis+1:].replace(' ','X')}")
     return get_first_correct_parenthesis(instring[last_parenthesis+1:])
 
-
 def get_third_correct_parenthesis(instring):
     num_parenthesis = 1
     first_parenthesis = instring.find('(')
@@ -112,43 +102,49 @@ def get_third_correct_parenthesis(instring):
     return get_second_correct_parenthesis(instring[last_parenthesis+1:])
 
 
+
 def clean_mimic_program(mimic_smtfile):
     with open(mimic_smtfile, 'r') as g:
         raw = g.readlines()[1]
     # raw = remove_divisions(raw)
-    raw = raw[raw.find("Bool"):]
+    raw = raw[raw.find("Bool"):] # need to adapt this for type changes
     let_values = []
     variables = 1
     found = True
     while found:
         found = False
         startind = raw.find(f"_let_{variables}")
-
+        
         if startind != -1:
             let_values.append(get_first_correct_parenthesis(raw[startind:]))
             variables += 1
-            found = True
-    last_portion = raw[raw.find(let_values[-1])+len(let_values[-1]):]
+            found =True
+    if len(let_values) > 0:
+        last_portion = raw[raw.find(let_values[-1])+len(let_values[-1]):]
+    else:
+        last_portion = raw
+        
     for i in range(len(let_values)):
         for j in range(len(let_values)):
-            let_values[j] = let_values[j].replace(
-                f"_let_{i+1} ", f"{let_values[i]} ")
-            let_values[j] = let_values[j].replace(
-                f"_let_{i+1})", f"{let_values[i]})")
+            let_values[j] = let_values[j].replace(f"_let_{i+1} ",f"{let_values[i]} ")
+            let_values[j] = let_values[j].replace(f"_let_{i+1})",f"{let_values[i]})")
 
+    
     for i in range(len(let_values)):
-        last_portion = last_portion.replace(
-            f"_let_{i+1} ", f"{let_values[i]} ")
-        last_portion = last_portion.replace(
-            f"_let_{i+1})", f"{let_values[i]})")
+        last_portion = last_portion.replace(f"_let_{i+1} ",f"{let_values[i]} ")
+        last_portion = last_portion.replace(f"_let_{i+1})",f"{let_values[i]})")
     ite = last_portion.find('ite')
-    tree_first = get_first_correct_parenthesis(last_portion[ite:])
-    tree_second = get_second_correct_parenthesis(last_portion[ite:])
-    tree_third = get_third_correct_parenthesis(last_portion[ite:])
-    final = f"(ite {tree_first} {tree_second} {tree_third})"
+    if ite != -1:
+        tree_first = get_first_correct_parenthesis(last_portion[ite:])
+        tree_second = get_second_correct_parenthesis(last_portion[ite:])
+        tree_third = get_third_correct_parenthesis(last_portion[ite:])
+        final = f"(ite {tree_first} {tree_second} {tree_third})"
+    else:
+        tree_first = get_first_correct_parenthesis(last_portion)
+        final = f"(ite True {tree_first} {tree_first})"
+
     with open('mnist_mimic_program.txt', 'w') as fp:
         fp.write(final)
-
 
 def classify_image(pixels):
     file = open("mnist_mimic_program.txt", "r")
@@ -193,23 +189,22 @@ def createFile(constraints_ind, tempfile):
     os.system(f'{cvc5path} --lang=sygus2 {tempfile} >mnist_mimic.smt2')
 
     runTime = time.time() - start_time
-
+    
     # runs through getStates function to use for mean calculation
-    return runTime
+    return runTime  
 
 
 def run_benchmark(n, iterate, runtime_data):
     for i in tqdm(range(len(iterate))):
         num_constraints = iterate[i]
-        arr_constraints = []  # size i in iterate
-
-        for i in range(1, 11):
-            arr_constraints = random.sample(range(0, 153), num_constraints)
+        arr_constraints = [] # size i in iterate
+        
+        for i in range(1,11):
+            arr_constraints = random.sample(range(0,153), num_constraints)
             timeRun = createFile(arr_constraints)
-            runtime_data.loc[i, num_constraints] = timeRun
-
+            runtime_data.loc[i,num_constraints] = timeRun
+    
     # runtime_data.to_csv('mnist_runtime.csv')
-
 
 def mimic_program_global_accuracy(T):
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
@@ -225,17 +220,17 @@ def mimic_program_global_accuracy(T):
 
     for i in tqdm(range(xtotest.shape[0])):
         ground_truth = ytotest[i]
-        if ground_truth == 7 or ground_truth == 1 or ground_truth == 0:  # add third class
+        if ground_truth == 7 or ground_truth == 1 or ground_truth == 0:  # add third class:
             total_predictions += 1
             program_outcome = classify_image(list(xtotest[i]))
             prediction = np.argmax(model_outcome[i])
-            if program_outcome:
+            if program_outcome == 2 or program_outcome == 1: # adapt these lines for code for ternary classification
                 truths += 1
             else:
                 falses += 1
-            if program_outcome == (ground_truth == 7):
+            if ((ground_truth == 7) and (program_outcome == 2)) or ((ground_truth == 1) and (program_outcome == 1)):
                 right_predictions_acc += 1
-            if program_outcome == (prediction == 7):
+            if ((prediction == 7) and (program_outcome == 2)) or ((prediction == 1) and (program_outcome == 1)):
                 right_predictions_rec += 1
 
         # if i % 200 == 0:
@@ -243,7 +238,9 @@ def mimic_program_global_accuracy(T):
             # print(f"\n***********\n Ground truth: {ground_truth}, Program outcome: {program_outcome}, NN outcome: {prediction} \n")
             # T.display_digit(xtotest[i])
             # print("\n***********\n")
-    return right_predictions_acc/total_predictions, right_predictions_rec/total_predictions
+    return right_predictions_acc/total_predictions, right_predictions_rec/total_predictions 
+
+    
 
 
 # def get_accuracy(num_constraints, T):
@@ -264,13 +261,13 @@ def main():
     # arr_constraints = random.sample(range(0,len(all_constraints)), num_constraints)
     # createFile(arr_constraints)
 
-    # UNDER CONSTRUCTION
+    ### UNDER CONSTRUCTION
     g = open("smtfiles/mnist_constraints.smt2", "rt")
     all_constraints = g.readlines()
     g.close()
-    total_num_constraints = len(all_constraints)-5  # -5 for safety
+    total_num_constraints = len(all_constraints)-5 # -5 for safety
 
-    T = TrainingInstance(10, (28, 28, 1))
+    T = TrainingInstance(10, (28,28,1))
     T.data_preparation()
     # with open('models/mnist_model.pkl', 'br') as fp:
     #     T.model = pickle.load(fp)
@@ -280,20 +277,20 @@ def main():
 
     global_accuracy = pd.DataFrame(columns=constraints)
     global_recall = pd.DataFrame(columns=constraints)
-    n = 10
+    n=10
     for num_constraints in tqdm(constraints):
         for i in tqdm(range(n)):
             tempfile = 'smtfiles/temp_mnist.smt2'
-            arr_constraints = random.sample(
-                range(0, total_num_constraints), num_constraints)
+            arr_constraints = random.sample(range(0,total_num_constraints), num_constraints)
             createFile(arr_constraints, tempfile)
             clean_mimic_program('mnist_mimic.smt2')
             acc, rec = mimic_program_global_accuracy(T)
-            global_accuracy.loc[i, num_constraints] = acc
-            global_recall.loc[i, num_constraints] = rec
+            global_accuracy.loc[i,num_constraints] = acc
+            global_recall.loc[i,num_constraints] = rec
             global_accuracy.to_csv(f'data/mnist_global_accuracy.csv')
             global_recall.to_csv(f'data/mnist_global_recall.csv')
 
-
+    
+    
 if __name__ == "__main__":
     main()
