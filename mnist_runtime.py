7f3a80476ebc2bba5bbe91a705929af950e2138a
Katrine Bjorner <kbjorner@kbjorner.localdomain>
2023-07-26 14:34:03 -0400

    tree depth and num nodes saved to csv

--- mnist_runtime.py
+++ mnist_runtime.py
@@ -1,4 +1,4 @@
-import time, os, yaml
+import time, os, yaml, subprocess
 import pandas as pd
 import random
 from tqdm import tqdm
@@ -28,35 +28,93 @@ def createFile(constraints_ind):
         cvc5path = yaml.load(cf, Loader=yaml.FullLoader)['cvc5']
 
     start_time = time.time()
-    os.system(f'{cvc5path} --lang=sygus2 {auxfile1} >{auxfile2}')
+
+    with open(auxfile2, 'w') as f_out:
+        subprocess.run([cvc5path, '--lang=sygus2', auxfile1], stdout=f_out)
+    
+    # Read the contents of auxfile2
+    with open(auxfile2, 'r') as f:
+        auxfile2_contents = f.read()
+
+    print(auxfile2_contents)
+
+    num_nodes = 0
+    target_word = "ite"
+    index = auxfile2_contents.find(target_word)
+    decision_tree1 = auxfile2_contents[index + len(target_word):]
+    decision_tree2 = auxfile2_contents[index + len(target_word):]
+
+    g_e = "<="
+    ge_index = decision_tree1.find(g_e)
+    while ge_index != -1:
+        num_nodes += 1
+        decision_tree1 = decision_tree1[ge_index + len(g_e):]
+        ge_index = decision_tree1.find(g_e)
+
+    let = "_let"
+    let_index = decision_tree2.find(let)
+    while let_index != -1:
+        num_nodes += 1
+        decision_tree2 = decision_tree2[let_index + len(let):]
+        let_index = decision_tree2.find(let)
+
+    print(num_nodes)
+
+    decision_tree = auxfile2_contents[index - 1:]
+    print(decision_tree)
+
+    tree_depth = count_tree_depth(decision_tree)
+    print(tree_depth)
+
     os.remove(auxfile1)
     os.remove(auxfile2)
 
     runTime = time.time() - start_time
     
     # runs through getStates function to use for mean calculation
-    return runTime  
+    return runTime, num_nodes, tree_depth
+
+def count_tree_depth(tree_str):
+    max_depth = 0
+    current_depth = 0
+
+    for char in tree_str:
+        if char == "(":
+            current_depth += 1
+            max_depth = max(max_depth, current_depth)
+        elif char == ")":
+            current_depth -= 1
+
+    return max_depth
 
 
-def run_benchmark(n, iterate, runtime_data):
+def run_benchmark(n, iterate, runtime_data, treedepth_data, numnodes_data):
     for i in tqdm(range(len(iterate))):
         num_constraints = iterate[i]
         arr_constraints = [] # size i in iterate
         
         for i in tqdm(range(1,11), leave=False):
             arr_constraints = random.sample(range(0,153), num_constraints)
-            timeRun = createFile(arr_constraints)
+            timeRun, numNodes, treeDepth = createFile(arr_constraints)
             runtime_data.loc[i,num_constraints] = timeRun
+            numnodes_data.loc[i,num_constraints] = numNodes
+            treedepth_data.loc[i,num_constraints] = treeDepth
     
     runtime_data.to_csv('data/mnist_runtime.csv')
+    numnodes_data.to_csv('data/mnist_numnodes.csv')
+    treedepth_data.to_csv('data/mnist_treedepth.csv')
 
 
 def main():
-    n = 10
-    iterate = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    # n = 10
+    n = 1
+    # iterate = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    iterate = [10]
     runtime_data = pd.DataFrame(columns=iterate, index=range(1,n+1))
-    run_benchmark(n, iterate, runtime_data)
-    mnist_runtime_graph( )
-    plt.show()
+    treedepth_data = pd.DataFrame(columns=iterate, index=range(1,n+1))
+    numnodes_data = pd.DataFrame(columns=iterate, index=range(1,n+1))
+    run_benchmark(n, iterate, runtime_data, treedepth_data, numnodes_data)
+    # mnist_runtime_graph()
+    # plt.show()
 if __name__ == "__main__":
     main()
--- pima_runtime.py
+++ pima_runtime.py
@@ -44,6 +44,63 @@ def createFile(constraints_ind, grammar_type):
 
         # os.system(f'{cvc5path} --lang=sygus2 {inputfile} >{outputfile}')
 
+        # Read the contents of auxfile2
+    with open(outputfile, 'r') as f:
+        auxfile2_contents = f.read()
+
+
+    num_nodes = 0
+    target_word = "ite"
+    index = auxfile2_contents.find(target_word)
+    decision_tree1 = auxfile2_contents[index + len(target_word):]
+    decision_tree2 = auxfile2_contents[index + len(target_word):]
+    decision_tree3 = auxfile2_contents[index + len(target_word):]
+    decision_tree4 = auxfile2_contents[index + len(target_word):]
+    decision_tree5 = auxfile2_contents[index + len(target_word):]
+
+    g_e = ">="
+    ge_index = decision_tree1.find(g_e)
+    while ge_index != -1:
+        num_nodes += 1
+        decision_tree1 = decision_tree1[ge_index + len(g_e):]
+        ge_index = decision_tree1.find(g_e)
+
+    l_e = "<="
+    le_index = decision_tree2.find(l_e)
+    while le_index != -1:
+        num_nodes += 1
+        decision_tree2 = decision_tree2[le_index + len(l_e):]
+        le_index = decision_tree2.find(l_e)
+
+    node_true = "true"
+    true_index = decision_tree3.find(node_true)
+    while true_index != -1:
+        num_nodes += 1
+        decision_tree3 = decision_tree3[true_index + len(node_true):]
+        true_index = decision_tree3.find(node_true)
+
+    node_false = "false"
+    false_index = decision_tree4.find(node_false)
+    while false_index != -1:
+        num_nodes += 1
+        decision_tree4 = decision_tree4[false_index + len(node_false):]
+        false_index = decision_tree4.find(node_false)
+
+    let = "_let"
+    let_index = decision_tree5.find(let)
+    while let_index != -1:
+        num_nodes += 1
+        decision_tree5 = decision_tree5[let_index + len(let):]
+        let_index = decision_tree5.find(let)
+
+    print(f"nodes: {num_nodes}")
+
+    decision_tree = auxfile2_contents[index - 1:]
+    print(decision_tree)
+
+    tree_depth = count_tree_depth(decision_tree)
+    print(f"depth: {tree_depth}")
+
 
 
     # os.system(f'{cvc5path} --lang=sygus2 {inputfile} >{outputfile}')
@@ -51,10 +108,31 @@ def createFile(constraints_ind, grammar_type):
     # os.remove(outputfile)
 
     runTime = time.time() - start_time    
-    return runTime  
+    return runTime, num_nodes, tree_depth
 
+def count_tree_depth(tree_str):
+    max_depth = 0
+    current_depth = 0
+    in_division = False
+    stack = []
 
-def run_benchmark(n, iterate, runtime_data):
+    for char in tree_str:
+        if char == "(":
+            stack.append(char)
+            current_depth += 1
+            max_depth = max(max_depth, current_depth)
+        elif char == ")":
+            if stack:
+                stack.pop()
+            current_depth -= 1
+        elif char == "/":
+            in_division = True
+        elif char == " " and in_division:
+            in_division = False
+
+    return max_depth
+
+def run_benchmark(n, iterate, runtime_data, numnodes_data, treedepth_data):
     for grammar_type in {"simple", "bootstrap"}:
         for num_constraints in tqdm(iterate):
             arr_constraints = [] # size i in iterate
@@ -62,19 +140,28 @@ def run_benchmark(n, iterate, runtime_data):
             for i in tqdm(range(1,n+1),leave=False):
                 arr_constraints = random.sample(range(0,153), num_constraints)
     #             print(arr_constraints)
-                timeRun = createFile(arr_constraints, grammar_type=grammar_type)
+                timeRun, numNodes, treeDepth = createFile(arr_constraints, grammar_type=grammar_type)
                 runtime_data.loc[i,num_constraints] = timeRun
+                numnodes_data.loc[i,num_constraints] = numNodes
+                treedepth_data.loc[i,num_constraints] = treeDepth
+
         
         runtime_data.to_csv(f'data/pima_runtime_{grammar_type}.csv')
+        numnodes_data.to_csv(f'data/pima_numnodes_{grammar_type}.csv')
+        treedepth_data.to_csv(f'data/pima_treedepth_{grammar_type}.csv')
 
 
 def main():
-    n = 10
-    iterate = [20, 40, 60, 80, 100, 120, 140]
+    # n = 10
+    n = 1
+    # iterate = [20, 40, 60, 80, 100, 120, 140]
+    iterate = [10]
     runtime_data = pd.DataFrame(columns=iterate, index=range(1,n+1))
-    run_benchmark(n, iterate, runtime_data)
-    pima_runtime_graph()
-    plt.show()
+    numnodes_data = pd.DataFrame(columns=iterate, index=range(1,n+1))
+    treedepth_data = pd.DataFrame(columns=iterate, index=range(1,n+1))
+    run_benchmark(n, iterate, runtime_data, numnodes_data, treedepth_data)
+    # pima_runtime_graph()
+    # plt.show()
 
 if __name__ == "__main__":
     main()
